413749c0448a8a3e0395ee2d9ce115fda7839fc4
Jose-Vela <joseeloyvo@gmail.com>
2023-05-23 01:34:04 -0600

    Se terminó de crear la pantalla (activity), donde se muestra el resultado del cálculo del IMC.
    Terminamos de desarrollar la App con los requerimientos y funcionalidades solicitadas.

--- app/src/main/java/com/example/imcapp/ResultIMCActivity.kt
+++ app/src/main/java/com/example/imcapp/ResultIMCActivity.kt
@@ -2,12 +2,69 @@ package com.example.imcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.example.imcapp.MainActivity.Companion.IMC_KEY
 
 class ResultIMCActivity : AppCompatActivity() {
+
+    private lateinit var tvResult: TextView
+    private lateinit var tvIMC: TextView
+    private lateinit var tvDescription: TextView
+    private lateinit var btnReCalculate: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result_imcactivity)
         val result:Double = intent.extras?.getDouble(IMC_KEY) ?: -1.0
+        initComponents()
+        initUI(result)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        btnReCalculate.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
+    }
+
+    private fun initUI(result: Double) {
+        tvIMC.text = result.toString()
+        when(result){
+            in 0.00..18.50 -> { // Bajo peso
+                tvResult.text = getString(R.string.titleUnderWeight)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.under_weight))
+                tvDescription.text = getString(R.string.descriptionUnderWeight)
+            }
+
+            in 18.51..24.99 -> { // Peso normal
+                tvResult.text = getString(R.string.titleNormalWeight)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.normal_weight))
+                tvDescription.text = getString(R.string.descriptionNormalWeight)
+            }
+
+            in 25.00..29.99 -> { // Sobrepeso
+                tvResult.text = getString(R.string.titleOverWeight)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.over_weight))
+                tvDescription.text = getString(R.string.descriptionOverWeight)
+            }
+
+            in 30.00..99.00 -> { // Obecidad
+                tvResult.text = getString(R.string.titleObesity)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.obesity))
+                tvDescription.text = getString(R.string.descriptionObesity)
+            }
+
+            else -> { // Error
+                tvResult.text = getString(R.string.error)
+                tvResult.setTextColor(ContextCompat.getColor(this, R.color.obesity))
+                tvDescription.text = getString(R.string.descriptionObesity)
+            }
+        }
+    }
+
+    private fun initComponents() {
+        tvResult = findViewById(R.id.tvResult)
+        tvIMC = findViewById(R.id.tvIMC)
+        tvDescription = findViewById(R.id.tvDescription)
+        btnReCalculate = findViewById(R.id.btnReCalculate)
     }
 }
